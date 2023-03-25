bf455364647af6992ea4fb4d57045db96afcb9d9
Leila Tolegenova <leilatheleph@gmail.com>
2023-03-25 23:21:33 +0600

    Initial Commit

---
+++ AskhanaAppPetProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AskhanaAppPetProject
+//
+//  Created by Dmitry Serebrov on 25.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
